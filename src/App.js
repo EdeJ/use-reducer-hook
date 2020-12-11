95b3e074d234b10b111b95bedb92651a8c27df94
Emiel de Jong <emieldejong@xs4all.nl>
2020-12-11 17:06:34 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from 'react'
 
 function App() {
+
+  const ACTIONS = {
+    ADD_TODO: 'add-todo'
+  }
+
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState('');
+
+  function reducer(todos, actions) {
+    switch (actions.type) {
+      case ACTIONS.ADD_TODO:
+        return [...todos, newTodo(actions.payload.name)];
+
+      default:
+        return todos;
+    }
+  }
+
+  function newTodo(name) {
+    return { id: Date.now(), name: name, complete: false }
+  }
+
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
+    setName('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
+    </form>
+  )
 }
 
-export default App;
+export default App
